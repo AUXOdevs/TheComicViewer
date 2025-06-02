dd12ae2ab1f86baf074ef5d089bc438d8307b1cd
MiguelAdrian <132788574+MiguelAdrian138@users.noreply.github.com>
2025-06-02 12:23:49 -0500

    creacion de mock y carrusel

---
+++ frontend/src/app/Landing/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import VistaBase from "@/Components/VistaBase/plantilla";
+
+const Landing = () => {
+  return (
+    <VistaBase
+      titulo="¡Bienvenido a TuComicViewer!"
+      mensajeBienvenida="Descubre los mejores mangas y cómics en un solo lugar. ¡Tu próxima aventura te espera!"
+    />
+  );
+};
+
+export default Landing;
